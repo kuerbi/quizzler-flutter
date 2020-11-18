21aa43318327a00bd34841b19fc597519d869724
Michael Golenia <mg@codegourmets.io>
2020-11-18 17:18:35 +0100

    Protect Question from changing and make question private in quizbrain

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,7 +1,7 @@
 import 'question.dart';
 
 class QuizBrain {
-  List<Question> questions = [
+  List<Question> _questions = [
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
@@ -28,4 +28,6 @@ class QuizBrain {
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
   ];
+
+  List<Question> get questions => _questions;
 }
